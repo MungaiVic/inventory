dc33aeeead0fc5d2faccb9a61c0819d79d94a062
MungaiVic <victornmungai@gmail.com>
2023-02-23 10:35:16 +0300

    Code refactor
    Replaced http status codes with fiber's inbuilt status codes

--- pkg/controllers/itemController.go
+++ pkg/controllers/itemController.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"net/http"
-
 	"github.com/MungaiVic/inventory/pkg/models"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -20,25 +18,25 @@ func GetItem(context *fiber.Ctx, db *gorm.DB) error {
 	itemID := context.Params("id")
 
 	if itemID == "" {
-		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+		context.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"message": "id cannot be empty",
 		})
 		return nil
 	}
 	if err := db.Where("id = ?", itemID).First(itemModel).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
-			context.Status(http.StatusNotFound).JSON(&fiber.Map{
+			context.Status(fiber.StatusNotFound).JSON(&fiber.Map{
 				"message": "Item not found.",
 			})
 			return nil
 		}
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+		context.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"message": "Could not get the item.",
 		})
 		return err
 	}
 
-	context.Status(http.StatusOK).JSON(&fiber.Map{
+	context.Status(fiber.StatusOK).JSON(&fiber.Map{
 		"message": "Book id fetched successfully",
 		"data":    itemModel,
 	})
@@ -49,19 +47,19 @@ func CreateItem(context *fiber.Ctx, db *gorm.DB) error {
 	itemModel := &models.Item{}
 	err := context.BodyParser(itemModel)
 	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+		context.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"message": "Request failed.",
 		})
 		return err
 	}
 	err = db.Create(&itemModel).Error
 	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+		context.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"message": "Could not create book",
 		})
 		return err
 	}
-	context.Status(http.StatusCreated).JSON(&fiber.Map{
+	context.Status(fiber.StatusCreated).JSON(&fiber.Map{
 		"message": "Created successfully",
 		"data":    itemModel,
 	})
